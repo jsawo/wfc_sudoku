6171d4084a93ec4af85ad4e51ef5c7f442001f07
Jacek Sawoszczuk <jacek.sawoszczuk@gmail.com>
2020-10-05 13:30:17 +0200

    initial commit

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public void SlotClicked(GameObject buttonGameObject = null)
+    {
+        Debug.Log($"Slot clicked - {buttonGameObject.name}");
+    }
+}
